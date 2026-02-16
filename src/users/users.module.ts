7e9cefb48b678ee8b893066b5acce16ec72d4193
coallfield <fisunov1999@gmail.com>
2026-02-16 19:42:32 +0300

    add jwt guard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,16 @@
-import {Module} from "@nestjs/common";
+import {forwardRef, Module} from "@nestjs/common";
 import {UsersController} from "./users.controller";
 import {UsersService} from "./users.service";
 import {usersProviders} from "./users.providers";
 import {RolesModule} from "../roles/roles.module";
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
     controllers: [UsersController],
     providers: [UsersService, ...usersProviders],
     imports: [
-        RolesModule
+        RolesModule,
+        forwardRef(() => AuthModule)
     ],
     exports: [
         UsersService,
